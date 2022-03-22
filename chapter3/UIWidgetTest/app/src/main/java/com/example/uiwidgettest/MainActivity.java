d96c76c89d442c84bd4d77e7df146998f7f34bbb
holyCloud <yang@hstc.edu.cn>
2022-03-22 15:56:41 +0800

    UIWidget 修订

--- chapter3/UIWidgetTest/app/src/main/java/com/example/uiwidgettest/MainActivity.java
+++ chapter3/UIWidgetTest/app/src/main/java/com/example/uiwidgettest/MainActivity.java
@@ -1,8 +1,6 @@
 package com.example.uiwidgettest;
 
-import android.app.AlertDialog;
 import android.app.ProgressDialog;
-import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,7 +8,6 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -24,7 +21,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button button = (Button) findViewById(R.id.button);
+        Button button = (Button) findViewById(R.id.bt1);
         editText = (EditText) findViewById(R.id.edit_text);
         imageView  = (ImageView) findViewById(R.id.image_view);
         progressBar = (ProgressBar) findViewById(R.id.progress_bar);
@@ -34,7 +31,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
-            case R.id.button:
+            case R.id.bt1:
                 ProgressDialog progressDialog = new ProgressDialog(MainActivity.this);
                 progressDialog.setTitle("This is ProgressDialog");
                 progressDialog.setMessage("Loading...");
